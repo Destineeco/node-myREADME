9c9798eaa47088ceac514592d85d378b6b1b121e
HP USER <Destmil.co@gmail.com>
2024-09-23 19:40:00 -0400

    updated markdownjs with license code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,7 +23,18 @@ export function renderLicenseBadge(license) {
   // TODO: Create a function that returns the license link
   // If there is no license, return an empty string
   function renderLicenseLink(license) {
-
+    if (!license) {
+      return '';
+    }
+    const licenseLinks = {
+      'MIT License': '[MIT License](https://opensource.org/licenses/MIT)',
+      'Apache License 2.0': '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)',
+      'GNU General Public License v3.0': '[GPL v3](https://opensource.org/licenses/GPL-3.0)',
+      'BSD 3-Clause License': '[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)',
+      'BSD 3-Clause License': '[CC0](https://creativecommons.org/publicdomain/zero/1.0/)',
+    };
+  
+    return licenseLinks[license] || '';
 
   }
 
@@ -34,7 +45,17 @@ export function renderLicenseBadge(license) {
   //TODO: Create a function that returns the license section of README
   // If there is no license, return an empty string
   function renderLicenseSection(license) {
-
+    if (license === 'MIT License') {
+      return '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    } else if (license === 'Apache License 2.0') {
+      return '![Apache 2.0 License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    } else if (license === 'GNU General Public License v3.0') {
+      return '![GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)';
+    } else if (license === 'BSD 3-Clause License') {
+      return '![BSD 3-Clause License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)';
+    } else {
+      return ''; 
+    }
 
   }
 
